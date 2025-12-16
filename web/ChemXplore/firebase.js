7227f6eb4db9e826a2abbd2d6d14cf8e17c62f2e
Allen Audrey Kish Leyble <qaaknleyble@tip.edu.ph>
2025-12-16 22:34:06 +0800

    Move Firebase config to env vars

--- web/ChemXplore/firebase.js
+++ web/ChemXplore/firebase.js
@@ -1,17 +1,12 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBBAsLaOvNQRKSEgq688VJu54EU_XMedz8",
-  authDomain: "chemxplore-ba0a5.firebaseapp.com",
-  projectId: "chemxplore-ba0a5",
-  storageBucket: "chemxplore-ba0a5.firebasestorage.app",
-  messagingSenderId: "598800564642",
-  appId: "1:598800564642:web:f4c831cf9128b0f462b2fb"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
